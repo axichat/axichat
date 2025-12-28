64ea3b9a2e0d4d92e85960bf78ac51a60dd1b3d4
eliot <eliot@axichat.com>
2025-12-28 13:27:52 -0800

    Fix bugs.

---
+++ lib/src/common/ui/link_action_dialog.dart
@@ -0,0 +1,50 @@
+import 'package:axichat/src/app.dart';
+import 'package:axichat/src/common/ui/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+enum LinkAction {
+  open,
+  copy,
+}
+
+Future<LinkAction?> showLinkActionDialog(
+  BuildContext context, {
+  required String title,
+  required String message,
+  required String openLabel,
+  required String copyLabel,
+  required String cancelLabel,
+}) {
+  return showShadDialog<LinkAction>(
+    context: context,
+    builder: (dialogContext) {
+      final pop = Navigator.of(dialogContext).pop;
+      return ShadDialog(
+        title: Text(
+          title,
+          style: context.modalHeaderTextStyle,
+        ),
+        actions: [
+          ShadButton.outline(
+            onPressed: () => pop(null),
+            child: Text(cancelLabel),
+          ).withTapBounce(),
+          ShadButton.secondary(
+            onPressed: () => pop(LinkAction.copy),
+            child: Text(copyLabel),
+          ).withTapBounce(),
+          ShadButton(
+            onPressed: () => pop(LinkAction.open),
+            child: Text(openLabel),
+          ).withTapBounce(),
+        ],
+        child: SelectableText(
+          message,
+          style: context.textTheme.small,
+          textAlign: TextAlign.start,
+        ),
+      );
+    },
+  );
+}
