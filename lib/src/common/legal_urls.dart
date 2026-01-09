e20d4700a22ce31382092549e7bf236bfff10e05
eliot <eliot@axichat.com>
2026-01-09 14:04:35 -0800

    Fix common bugs.

---
+++ lib/src/common/legal_urls.dart
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2025-present Eliot Lew, Axichat Developers
+
+const String termsUrl = 'https://axichat.com/terms.pdf';
+const String privacyUrl = 'https://axichat.com/privacy.pdf';
